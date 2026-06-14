e150e66cf0b76f13a277448eaf44b99fe07fa8da
eyano3 <edemyanov222@gmail.com>
2026-06-14 16:20:54 -0500

    Built driver skeleton/functionality

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,8 +1,28 @@
 package org.example;
+import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Write a driver program for the Converter class here!");
-    System.out.println("You can delete these print statements and replace them with your code.");
+
+    //Initialize our ounce converter and input reader
+    Converter converterOunces = new Converter();
+    Scanner userReader = new Scanner(System.in);
+
+    //Welcome User and collect input
+    System.out.println("Welcome to the Ounce Convertor.");
+    System.out.println("Please enter the amount of Ounces: ");
+    String userQuantityOunces = userReader.nextLine();
+    System.out.println("Your input was received.");
+
+    //Convert User input
+    float userQuantityPounds = converterOunces.toPounds(userQuantityOunces);
+    String userQuantityPoundsAndOunces = converterOunces.toPoundsAndOunces(UserQuantityOunces);
+
+    //Return requested output
+    System.out.println(userQuantityOunces + " Ounces is " + userQuantityPounds "Pounds.");
+    System.out.println(userQuantityOunces + " Ounces is " + userQuantityPoundsAndOunces + ".");
+    System.out.println("Thank you for using the Ounce Converter.")
+
+
   }
 }
